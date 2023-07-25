91f598cdbcb685688dc013c2b5e2019b59a343a1
Ratnesh Yadav <106252196+Ratnesh0@users.noreply.github.com>
2023-07-26 00:37:47 +0530

    mcu pager

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-# Medical Pager Chat App
-
-![Chat Application](https://i.ibb.co/hsvcw4V/image.png)
+# MCU Pager Chat App
 
 ## Introduction
 This is a code repository for the corresponding video tutorial. 
--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -30,7 +30,7 @@ const Auth = () => {
 
         const URL = 'http://localhost:5000/auth';
         
-        // const URL = 'https://medical-pager.herokuapp.com/auth';
+        // const URL = 'https://MCU-pager.herokuapp.com/auth';
 
         const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
             username, password, fullName: form.fullName, phoneNumber, avatarURL,
